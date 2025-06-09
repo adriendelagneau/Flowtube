25799184edae643a92cefbf977828232d735581d
b166er <delagneauadrien@yahoo.fr>
2025-06-09 22:57:18 +0200

    before webhook

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -5,7 +5,6 @@ const StudioPage = async () => {
   return (
     <div className="flex flex-col gap-y-6 pt-2.5">
       <div className="px-4">
-     
         <h1 className="text-2xl font-bold">Channel content</h1>
         <p className="text-muted-foreground text-xs">
           Manage your channel content and videos
