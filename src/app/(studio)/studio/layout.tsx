c0701fbe4689dc037465ddbe6349bc202799b92f
b166er <delagneauadrien@yahoo.fr>
2025-06-09 23:11:29 +0200

    before webhook

--- src/app/(studio)/studio/layout.tsx
+++ src/app/(studio)/studio/layout.tsx
@@ -1,25 +1,25 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
-import { getUser } from "@/lib/auth/auth-session";
+// import { getUser } from "@/lib/auth/auth-session";
 
 import { StudioNavbar } from "./components/layout/navbar/studio-navbar";
-import { StudioSidebar } from "./components/layout/sidebar/studio-sidebar";
+// import { StudioSidebar } from "./components/layout/sidebar/studio-sidebar";
 
 export default async function StudioLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const user = await getUser();
-  if (!user) {
-    return <div>Unauthorized</div>; // ✅ Handle missing user properly
-  }
+  // const user = await getUser();
+  // if (!user) {
+  //   return <div>Unauthorized</div>; // ✅ Handle missing user properly
+  // }
 
   return (
     <SidebarProvider>
       <div className="w-full">
         <StudioNavbar />
         <div className="flex min-h-screen pt-[4rem]">
-          <StudioSidebar user={{ ...user, image: user.image ?? null }} />
+          {/* <StudioSidebar user={{ ...user, image: user.image ?? null }} /> */}
           <main className="flex-1 overflow-y-auto">{children}</main>
         </div>
       </div>
